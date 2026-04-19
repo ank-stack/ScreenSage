b595b737a6cad1a67ace56f81fd0ba7ac7730add
ank-stack <dasankan918@gmail.com>
2026-04-19 19:35:47 +0530

    Added: Image to Base64 and Base64 to Image func

--- screenshot.py
+++ screenshot.py
@@ -1,4 +1,6 @@
-import pyautogui, os
+import pyautogui
+import os
+import base64
 
 def capture_screen(count):
     folder = "data"
@@ -11,5 +13,18 @@ def capture_screen(count):
 
     img.save(path)
 
+    return path
+
+def image_to_base64(img_path):
+    with open(img_path,"rb") as img_file:
+        encoded_string = base64.b64encode(img_file.read())
+        return encoded_string.decode("utf-8")
+    
+def base64_to_image(b64_string, output):
+    with open(output,"wb") as img_file:
+        img_file.write(base64.b64decode(b64_string))
+
 if __name__ == "__main__":
-    capture_screen(1)
+    img_1 = capture_screen(1)
+    base = image_to_base64(img_1)
+    base64_to_image(base, "output.png")
