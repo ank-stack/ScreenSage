e9f028c83db4d2dd65705f1c358ba1048f8bcf72
ank-stack <dasankan918@gmail.com>
2026-04-21 15:25:35 +0530

    update: Added Genai and anthropic API

--- ai_clients.py
+++ ai_clients.py
@@ -1,13 +1,19 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+from google import genai
+from google.genai import types
+from anthropic import Anthropic
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+client_1 = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+client_2 = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
+client_3 = Anthropic(api_key=os.getenv('ANTHROPIC_API_KEY'))
 
-def get_response_b64(img_b64):
-    response = client.responses.create(
+# OpenAI API
+def openai_b64(img_b64):
+    response = client_1.responses.create(
         model="gpt-4o-mini",
         input=[
             {
@@ -30,7 +36,7 @@ def get_response_b64(img_b64):
 
 # Local Files Not Supported
 def get_response_img(img):
-    response = client.responses.create(
+    response = client_1.responses.create(
         model="gpt-4o-mini",
         input=[
             {
@@ -50,3 +56,46 @@ def get_response_img(img):
     )
     
     return(response.output_text)
+
+# Genai API
+def genai_b64(img_64):
+    response = client_2.models.generate_content(
+        model='gemini-2.0-flash',
+        contents=[
+            types.Part.from_bytes(
+                data=img_64,
+                mime_type='image/jpeg'
+            ),
+            "Give one word answer in JSON"
+        ]
+    )
+
+    return(response.text)
+
+# Claude API
+def anthropic_b64(img_64):
+    message = client_3.messages.create(
+        model="claude-sonnet-4-6",
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": "image/png",
+                            "data": img_64,
+                        },
+                    },
+                    {
+                        "type": "text", 
+                        "text": "Give one word answer in JSON."
+                    },
+                ],
+            }
+        ],
+    )
+
+    return(message)
